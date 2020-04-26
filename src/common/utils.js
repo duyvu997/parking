1a8e3d07e959fcf5b656bd3adcdafc1d3e94e469
duy.vu <duy.vu@pycogroup.com>
2020-04-26 23:25:16 +0700

    dev function to  check valid capacity

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,4 +1,10 @@
+const MAX_ACCEPTABLE_CAPACITY = 100000;
 const isFull = function (capacity, data) {
     return data.length == capacity;
 }
+const isValidCapacity = function (capacity) {
+    const numberParsed = parseInt(capacity, 10)
+    return numberParsed > 0 && numberParsed < MAX_ACCEPTABLE_CAPACITY
+}
 module.exports.isFull = isFull;
+module.exports.isValidCapacity = isValidCapacity;
