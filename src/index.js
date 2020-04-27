8243ca11549b4fb027df9032c574915e5875643b
duy.vu <duy.vu@pycogroup.com>
2020-04-27 11:35:25 +0700

    ensure the create_parking_lot operator start first

--- src/index.js
+++ src/index.js
@@ -17,19 +17,27 @@ const main = () => {
                     isCreated = true;
                     break;
                 case constant.PARK:
-                    parkingLot.park(event, parkArea);
+                    isCreated ? parkArea = parkingLot.park(event, parkArea) :
+                        console.log("please input the create parking lot first")
                     break;
                 case constant.LEAVE:
-                    console.log("Registration number KA-01-HH-3141 with Slot Number 6 is free with Charge 30");
+                    isCreated ?
+                        parkArea = parkingLot.leave(event, parkArea) :
+                        console.log("please input the create parking lot first")
+
                     break;
                 case constant.STATUS:
-                    console.log(`
+                    isCreated ?
+                        console.log(`
                 Slot No.        Registration No.
                 1                KA-01-HH-1234
                 2                KA-01-HH-9999
                 3                KA-01-BB-0001
                 4                KA-01-HH-7777
-                5                KA-01-HH-2701`)
+                5                KA-01-HH-2701`) :
+                        console.log("please input the create parking lot first")
+
+                    break;
                 default:
                     console.log("please input a valid event:  create_parking_lot | park | leave | status")
             }
